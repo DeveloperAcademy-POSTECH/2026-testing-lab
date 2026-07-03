925c57d3f758651fcb4032a38af7dbf839a82f43
sun <sts07190@naver.com>
2026-07-03 15:18:56 +0900

    ADD: initial commit

---
+++ CalculatorTesting/CalculatorTesting/CalculatorTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorTestingApp.swift
+//  CalculatorTesting
+//
+//  Created by sun on 7/3/26.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalculatorTesting/CalculatorTesting/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CalculatorTesting
+//
+//  Created by sun on 7/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
