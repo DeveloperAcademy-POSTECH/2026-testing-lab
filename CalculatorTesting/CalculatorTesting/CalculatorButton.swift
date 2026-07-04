1c3bc95281f126814b025ac45ae8033f6dcfa9b0
sun <sts07190@naver.com>
2026-07-04 22:24:15 +0900

    FEAT: add calculator button model and UI
    계산기 버튼 모델 및 버튼 UI 구현

---
+++ CalculatorTesting/CalculatorTesting/CalculatorButton.swift
@@ -0,0 +1,81 @@
+//
+//  CalculatorButton.swift
+//  CalculatorTesting
+//
+//  Created by sun on 7/3/26.
+//
+
+import SwiftUI
+
+enum CalculatorButton: Hashable {
+    case digit(Int)
+    case decimal
+    case equals
+
+    case add
+    case subtract
+    case multiply
+    case divide
+
+    case percent
+    case plusMinus
+    case clear
+    case backspace
+
+    var title: String {
+        switch self {
+        case .digit(let number):
+            return "\(number)"
+        case .decimal:
+            return "."
+        case .equals:
+            return "="
+        case .add:
+            return "+"
+        case .subtract:
+            return "−"
+        case .multiply:
+            return "×"
+        case .divide:
+            return "÷"
+        case .percent:
+            return "%"
+        case .plusMinus:
+            return "+/−"
+        case .clear:
+            return "AC"
+        case .backspace:
+            return "⌫"
+        }
+    }
+
+    var operation: CalculatorOperation? {
+        switch self {
+        case .add:
+            return .add
+        case .subtract:
+            return .subtract
+        case .multiply:
+            return .multiply
+        case .divide:
+            return .divide
+        default:
+            return nil
+        }
+    }
+
+    var backgroundColor: Color {
+        switch self {
+        case .add, .subtract, .multiply, .divide, .equals:
+            return .orange
+        case .clear, .percent, .plusMinus, .backspace:
+            return Color.gray.opacity(0.8)
+        default:
+            return Color(white: 0.2)
+        }
+    }
+
+    var foregroundColor: Color {
+        .white
+    }
+}
